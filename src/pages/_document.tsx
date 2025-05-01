4575fd5cdacb87af094a2afbb914f0a2b07d5871
Constantine Krenteras <constantinos_krenteras@brown.edu>
2025-05-01 19:30:28 -0400

    changed title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <title>Materials Station</title>
+        <title>Material Circulation</title>
       </Head>
       <body className="antialiased">
         <Main />
