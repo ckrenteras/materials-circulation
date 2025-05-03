a2b10fa2a0c15370aab9bf33c08ba4168d3e88aa
Constantine Krenteras <constantinos_krenteras@brown.edu>
2025-05-03 19:59:03 -0400

    home button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Link href="/" className="fixed top-4 left-4 z-50">
+        <Image
+          src="/favicon.ico"
+          alt="Home"
+          width={32}
+          height={32}
+          className="hover:opacity-80 cursor-pointer"
+        />
+      </Link>
+      <Component {...pageProps} />
+    </>
+  );
 }
