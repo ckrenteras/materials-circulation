68ea9cf12973adc59a0fb78526784f8b61f7417f
Constantine Krenteras <constantinos_krenteras@brown.edu>
2025-05-03 11:53:40 -0400

    text clors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       >
         <p className="text-lg absolute left-[-5rem] top-[-2.5rem]">Welcome to</p>
         <h1 className="text-6xl font-bold">Materials Circulation</h1>
-        <p className="text-lg absolute right-[-4rem] top-[3.5rem]">@ the BDW</p>
+        <p className="text-lg absolute right-[-3rem] top-[3.5rem]">@ the BDW</p>
       </motion.div>
 
       <motion.div
@@ -20,7 +20,7 @@ export default function Home() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 1, duration: 1 }}
       >
-        <p className="text-base text-gray-700 mb-8">
+        <p className="text-base text-gray-800 mb-8">
           Track and share your design workshop materials here.
         </p>
 
@@ -31,7 +31,7 @@ export default function Home() {
             </button>
           </Link>
           <Link href="/about">
-            <button className="bg-gray-200 px-4 py-2 rounded hover:bg-gray-300">
+            <button className="bg-gray-400 px-4 py-2 rounded hover:bg-gray-500">
               About
             </button>
           </Link>
